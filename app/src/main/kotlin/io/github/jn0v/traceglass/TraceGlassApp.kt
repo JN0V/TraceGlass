9b57cef85963d200d4430df69c6e1b22f93a0090
JN0V <jn0v@jeanjean.eu>
2026-02-08 12:30:16 +0100

    feat: F-Droid reproducibility configuration (Story 1.2)
    - Fix vectorDrawables.generatedDensities (clear mutable set)
    - Disable PNG crunching via gradle.properties
    - Initialize Koin with empty modules in TraceGlassApp
    - assembleRelease ✓

--- app/src/main/kotlin/io/github/jn0v/traceglass/TraceGlassApp.kt
+++ app/src/main/kotlin/io/github/jn0v/traceglass/TraceGlassApp.kt
@@ -1,5 +1,15 @@
 package io.github.jn0v.traceglass
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class TraceGlassApp : Application()
+class TraceGlassApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@TraceGlassApp)
+            modules(emptyList())
+        }
+    }
+}
